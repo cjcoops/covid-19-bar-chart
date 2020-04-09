324b6b016fd51dfdc8b11f0c8cabdeda23d851ef
chris.cooper-git-at-541938338397 <chris.cooper@koresoftware.com>
2020-04-08 19:40:17 -0700

    Add pages

---
+++ scatter-plot.js
@@ -0,0 +1,85 @@
+import {
+  select,
+  csv,
+  scaleLinear,
+  max,
+  scaleBand,
+  axisLeft,
+  axisBottom,
+  format,
+} from "d3";
+
+module.exports = () => {
+  const titleText =
+    "Number of Cases vs Population";
+  const xAxisLabelText = "Number of Cases";
+
+  select("svg").selectAll("*").remove();
+  const svg = select("svg");
+
+  const width = +svg.attr("width");
+  const height = +svg.attr("height");
+
+  const render = (data) => {
+    select("svg").selectAll("*").remove();
+
+    const xValue = (d) => d["number_of_cases"];
+    const yValue = (d) => d.province;
+    const margin = { top: 50, right: 40, bottom: 77, left: 260 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const xScale = scaleLinear()
+      .domain([0, max(data, xValue)])
+      .range([0, innerWidth]);
+
+    const yScale = scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1);
+
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const xAxisTickFormat = (number) => format("~s")(number);
+
+    const xAxis = axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+
+    g.append("g")
+      .call(axisLeft(yScale))
+      .selectAll(".domain, .tick line")
+      .remove();
+
+    const xAxisG = g
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0,${innerHeight})`);
+
+    xAxisG.select(".domain").remove();
+
+    xAxisG
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("y", 50)
+      .attr("x", innerWidth / 2)
+      // .attr("fill", "white")
+      .text(xAxisLabelText);
+
+    g.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", (d) => yScale(yValue(d)))
+      .attr("width", (d) => xScale(xValue(d)))
+      .attr("height", yScale.bandwidth());
+
+    g.append("text").attr("class", "title").attr("y", -15).text(titleText);
+  };
+
+  csv(require("./assets/data.csv")).then((data) => {
+    render(data);
+  });
+};
